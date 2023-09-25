878de9d4ce5a8379d4a432ce23228013153aae43
Sean Galusz <sgalusz@gmail.com>
2023-09-24 20:40:16 -0400

    Excercise 4 now works with multiple parameters

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,14 @@
-const removeFromArray = function(input,toRemove1,toRemove2 = []) {
-    let result =[];
-    if (toRemove2 === "")
+const removeFromArray = function(input,...toRemove) {
+    let result = [];
+  
+    for (let argCounter = 0; x < toRemove.length - 1; argCounter++)
     {
-        for (let x = 0; x < input.length; x++){
-        if (!(input[x] === toRemove1)){
-            result.push(input[x]);
-        }
-        }
-    } else {
-        for (let x = 0; x < input.length; x++){
-            if (!(input[x] === toRemove1 || input[x] === toRemove2)){
+        for (let inputCounter = 0; x , input.length - 1; inputCounter++)
+        {
+            if (!(input[inputCounter] === toRemove[argCounter])){
                 result.push(input[x]);
             }
-            }
+        }
     }
 
     return result;
